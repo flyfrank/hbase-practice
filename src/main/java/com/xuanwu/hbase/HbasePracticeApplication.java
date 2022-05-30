b6fd49baf0d6a3a11a2a3fdc1b93c39f8bb74c71
huyaoke <huyaoke@wxchina.com>
2022-05-30 21:09:54 +0800

    first commit

---
+++ src/main/java/com/xuanwu/hbase/HbasePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.xuanwu.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbasePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbasePracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/xuanwu/hbase/config/HBaseConfig.java
@@ -0,0 +1,9 @@
+package com.xuanwu.hbase.config;
+
+/**
+ * @author YaokeHu
+ * @description
+ * @date 2022-05-30 20:16
+ **/
+public class HBaseConfig {
+}
